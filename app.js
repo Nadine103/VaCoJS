7ced164e80419475fd3f330196fbf55a34edd29e
Nadine101 <nadinet026@gmail.com>
2022-07-12 21:58:41 -0600

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let userage = 18;
+let userAge = 16;
+let userage = 19;
+let userAge_is_Subscribe = 18;
+"userAge_is_Subscribe" = true;
+"userAge_not_Subscribe" = false;
+if( userAge >= 18 === true) {
+    console.log("the user is 18 or older and is Subscribe");
+} else {
+    console.log(" the user is younger than 18 and is not_Subribe")
+}
